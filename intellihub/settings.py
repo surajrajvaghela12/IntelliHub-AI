60f7e5f0a8b2337782287fd413201a5cd1a538bf
surajrajvaghela12 <surajrajvaghela12@gmail.com>
2026-08-02 14:51:53 +0530

    Embed full dark mode glassmorphic styling into base.html and landing.html for guaranteed zero-downtime UI rendering on Vercel

--- intellihub/settings.py
+++ intellihub/settings.py
@@ -112,8 +112,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_AUTOREFRESH = True
